22fb2c0ba5aff3e0baac9405af6adba5daacc347
Ashwani Choudhary <Lucky@ashwanis-mac-mini.local>
2016-05-20 12:06:24 +0530

    Initial Commit

---
+++ JsonParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonParser
+//
+//  Created by Ashwani Choudhary on 20/05/16.
+//  Copyright © 2016 Ashwani Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
